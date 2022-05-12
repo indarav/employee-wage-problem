68fef2d3fe3d7e99f1ce3b6d51a70b6a1d5bbd8e
aravind <aravindkamuni9@gmail.com>
2022-05-12 10:22:32 +0530

    [aravind] add: employee wage problem with all use cases

---
+++ forLoopEmployeeWage.js
@@ -0,0 +1,37 @@
+
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+
+        default:
+            return 0;
+    }
+
+}
+
+const numOfWorkingDays = 20;
+let empHrs = 0;
+for (let days = 0; numOfWorkingDays < 20; days++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log('wage of the employee: ' + empWage);
+
+
+
+
